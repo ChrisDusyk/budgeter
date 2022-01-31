772c0ea32ad4de3d4d5d2c98da43f00224714216
Chris Dusyk <chrisd@iqmetrix.com>
2022-01-30 22:04:41 -0600

    Better data

--- src/pages/first-chart/FIrstChart.tsx
+++ src/pages/first-chart/FIrstChart.tsx
@@ -1,17 +1,20 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from "recharts";
+
 const data = [
-	{ name: "Page A", uv: 400, pv: 2400, amt: 2400 },
-	{ name: "Page B", uv: 350, pv: 2500, amt: 2500 },
-	{ name: "Page C", uv: 200, pv: 1000, amt: 1500 },
+	{ name: "Pizza", amount: 39.99 },
+	{ name: "J&A", amount: 68.89 },
+	{ name: "Bomb", amount: 80.94 },
 ];
 
 export function FirstChart() {
 	return (
 		<LineChart width={600} height={400} data={data}>
-			<Line type="monotone" dataKey="uv" stroke="#8884d8" />
+			<Line type="monotone" dataKey="amount" stroke="#8884d8" />
 			<CartesianGrid stroke="#ccc" />
 			<XAxis dataKey="name" />
 			<YAxis />
+			<Tooltip />
+			<Legend />
 		</LineChart>
 	);
 }
