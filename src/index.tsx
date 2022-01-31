6e7dd5c16b023a570a7b2781214f2923c1290263
Chris Dusyk <chris.dusyk@gmail.com>
2022-01-31 14:46:06 -0600

    Set up basic auth interactions

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Auth0Provider domain="dusyk-dev.us.auth0.com" clientId="4ulfvwuOXexlbPCfJgC7gEBAmTUkHhkp" redirectUri={window.location.origin}>
+			<App />
+		</Auth0Provider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
