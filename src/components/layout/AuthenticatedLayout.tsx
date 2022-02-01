5d23191c94b138b63312684a2269916de29f2d09
Chris Dusyk <chris.dusyk@gmail.com>
2022-02-01 10:25:03 -0600

    Clean up some styling bugs

--- src/components/layout/AuthenticatedLayout.tsx
+++ src/components/layout/AuthenticatedLayout.tsx
@@ -1,21 +1,23 @@
+import React from "react";
 import { Box, Button, Header, Main, Nav } from "grommet";
 import { Home } from "grommet-icons";
-import React from "react";
 import { LogoutButton, Profile } from "..";
 
 export const AuthenticatedLayout: React.FC<React.PropsWithChildren<unknown>> = ({ children }: React.PropsWithChildren<unknown>) => {
 	return (
-		<Box align="start" justify="start" border={{ color: "brand", size: "medium" }} fill>
-			<Header background="light-4" fill="horizontal" pad="small">
-				<Button icon={<Home />} hoverIndicator />
-				<Nav direction="row">
-					<Profile />
-					<LogoutButton />
-				</Nav>
-			</Header>
-			<Main border={{ color: "red", size: "medium" }} pad="medium" fill>
-				{children}
-			</Main>
+		<Box align="start" justify="start" border={{ color: "brand", size: "medium" }}>
+			<>
+				<Header background="light-4" fill="horizontal" pad="small">
+					<Button icon={<Home />} hoverIndicator />
+					<Nav direction="row">
+						<Profile />
+						<LogoutButton />
+					</Nav>
+				</Header>
+				<Main border={{ color: "red", size: "medium" }} pad="medium">
+					{children}
+				</Main>
+			</>
 		</Box>
 	);
 };
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grommet } from "grommet";
+import { grommet } from "grommet/themes";
 import { useAuth0 } from "@auth0/auth0-react";
 import { UnauthenticatedLayout } from "./UnauthenticatedLayout";
 import { AuthenticatedLayout } from "./AuthenticatedLayout";
@@ -7,5 +8,5 @@ import { AuthenticatedLayout } from "./AuthenticatedLayout";
 export const Layout: React.FC<React.PropsWithChildren<unknown>> = ({ children }: React.PropsWithChildren<unknown>) => {
 	const { isAuthenticated } = useAuth0();
 
-	return <Grommet full>{isAuthenticated ? <AuthenticatedLayout>{children}</AuthenticatedLayout> : <UnauthenticatedLayout />}</Grommet>;
+	return <Grommet theme={grommet}>{isAuthenticated ? <AuthenticatedLayout>{children}</AuthenticatedLayout> : <UnauthenticatedLayout />}</Grommet>;
 };
