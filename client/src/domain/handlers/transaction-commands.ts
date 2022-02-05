b064a31edb8b821cadd7f302ae1bb65f7f7d2ddc
Chris Dusyk <chris.dusyk@gmail.com>
2022-02-04 22:09:56 -0600

    🚧 stubbed out saveNewTransaction handler

--- client/src/domain/handlers/transaction-commands.ts
+++ client/src/domain/handlers/transaction-commands.ts
@@ -1,4 +1,21 @@
-import { ResultPromise } from "functional-ts-primitives";
+import { Result, ResultPromise } from "functional-ts-primitives";
 import type { TransactionToCreate, Error, Transaction } from "../models";
 
-export const saveNewTransactionAsync = (newTransaction: TransactionToCreate): ResultPromise<Transaction, Error> => {};
+export const saveNewTransactionAsync = (newTransaction: TransactionToCreate): ResultPromise<Transaction, Error> => {
+	return Result.successAsync<Transaction, Error>(() =>
+		Promise.resolve({
+			id: 0,
+			amount: 10.0,
+			category: {
+				id: 1,
+				label: "Groceries",
+			},
+			account: {
+				id: 2,
+				name: "Joint account",
+				owner: "Both",
+			},
+			description: "Co-op",
+		})
+	);
+};
